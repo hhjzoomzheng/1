3577022dabb87af82665e68dec994e12c2c0bb0e
hhj <1545701274@qq.com>
2019-05-24 15:40:03 +0800

    1

---
+++ src/staticTest/test.java
@@ -0,0 +1,29 @@
+package staticTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.NameAlreadyBoundException;
+
+public class test{
+	public static  List<String> name = new ArrayList<>();
+	 
+	 static {
+		 
+	 }
+
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+	}
+	
+}
+
+ class Test3 {
+	
+	public static void main(String[] args) {
+		test test = new test();
+		System.out.println(test.name);
+	}
+}
